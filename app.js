8bb3934f59776983a0dec36e6c98cb681b687e7b
rchouhani <rchouhani@me.com>
2025-05-27 22:39:57 +0200

    ADD : connection database and some dependencies

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
+//npm install pg dotenv
+//modification du .env
+
 require('dotenv').config();
 
 const { Pool } = require('pg');
+const sql = require('sql');
+sql.setDialect('postgres');
+
+const user = sql.define({
+  name: 'user',
+  columns: ['id', 'name', 'email', 'lastLogin']
+});
+
+const query = user
+    .select(user.id)
+    .from(user)
+    .where(
+      user.name.equals('boom').and(user.id.equals(1))
+    )
 
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 
 const pool = new Pool({
   host: PGHOST,
   database: PGDATABASE,
-  username: PGUSER,
+  user: PGUSER,
   password: PGPASSWORD,
   port: 5432,
   ssl: {
@@ -25,4 +42,26 @@ async function getPgVersion() {
   }
 }
 
-getPgVersion();
+getPgVersion().catch(err => {
+console.error('Erreur lors de la connexion à la base de données:', err);
+});
+
+// //Installation de npm install @neondatabase/serverless
+// //Rajout d'un .env avec les infos du server
+// require("dotenv").config();
+
+// const http = require("http");
+// const { neon } = require("@neondatabase/serverless");
+
+// const sql = neon(process.env.DATABASE_URL);
+
+// const requestHandler = async (req, res) => {
+//   const result = await sql`SELECT version()`;
+//   const { version } = result[0];
+//   res.writeHead(200, { "Content-Type": "text/plain" });
+//   res.end(version);
+// };
+
+// http.createServer(requestHandler).listen(3000, () => {
+//   console.log("Server running at http://localhost:3000");
+// });
