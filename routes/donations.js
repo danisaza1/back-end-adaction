cb79c827cfd17e50b318cff4efc4d74a447d4fa9
petronela <delyoara@yahoo.fr>
2025-06-03 23:19:03 +0200

    Add POST functionality linked to the frontend; GET endpoint for points to be added

--- routes/donations.js
+++ routes/donations.js
@@ -2,41 +2,31 @@ const express = require('express');
 const router = express.Router();
 const pool = require("../database");
 
-// const database = [];
-// router.get("/donations", async (req, res) => {
-// res.status(200).json({ message: "Collecte enregistrée avec succès." });
-//  try {
-//     const result = await pool.query("SELECT * FROM donations ORDER BY created_at DESC;");
-//     console.log()
-//     res.json({ data: result.rows });
-//   } catch (error) {
-//     console.error("Erreur dans /donations (GET):", error);
-//     res.status(500).json({ error: "Erreur serveur" });
-//   }
-// });
-
 router.post("/donations", async (req, res) => {
   // res.send('Donation reçue');
- console.log('req.body outside =', req.body);
-   const { asso_name, points, volunteerId } = req.body;
-   try {
-      console.log('req.body =', req.body);
-  // const dataD = { asso_name, points, volunteer_id} = req.body;
-
-  // const { date, cityName, wasteTypes } = req.body;
-//  const asso_name = "Ocean Cleanup";
-//     const points = 100;
-//     const volunteer_id = 2;
- const query = `INSERT INTO donations (asso_name, points, volunteer_id) VALUES ($1, $2, $3)`;
+  console.log('req.body outside =', req.body);
+  const { asso_name, points, volunteerId } = req.body;
+  try {
+    console.log('req.body =', req.body);
+    
+    const query = `INSERT INTO donations (asso_name, points, volunteer_id) VALUES ($1, $2, $3)`;
     const result = await pool.query(query, [asso_name, points, volunteerId]);
     res.json({ message: 'Donation reçue', result });
-}
-catch (error) {
-  console.error("Erreur dans /donations:", error);
-  res.status(500).json({ error: "Erreur serveur" });
-}
+  }
+  catch (error) {
+    console.error("Erreur dans /donations:", error);
+    res.status(500).json({ error: "Erreur serveur" });
+  }
 })
 
+// router.get("/donations/:id", async (req, res) => {
+// const volunteerId = req.params.id; 
+//   const result = await pool.query(
+//     "SELECT SUM(points) AS total_points FROM donations WHERE volunteer_id = $1",
+//     [volunteerId]
+//   )
+//   res.json({ totalPoints: result.rows[0].total_points || 0 });
+// });
 module.exports = router;
 
 
