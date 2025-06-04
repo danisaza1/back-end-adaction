d54896d948418ae212d5f4c437be98a1aa5fef35
rchouhani <rchouhani@me.com>
2025-06-04 22:32:00 +0200

    ADD : A route which works and update

--- routes/profil.js
+++ routes/profil.js
@@ -1,51 +1,38 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pool = require('../database');
+const pool = require("../database");
 
-
-router.get('/profil', async (req, res) => {
-    try {
-const volTable = "SELECT firstname, lastname, location FROM volunteers WHERE id=2";
-const result = await pool.query(volTable); 
-res.json(result.rows)
-console.log(result.rows);
-    } catch (error) {
-        console.error('Erreur dans /profil:', error);
-        res.status(500).json({ error: 'Erreur serveur' });
-    };
+router.get("/profil", async (req, res) => {
+  try {
+    const volTable =
+      "SELECT firstname, lastname, location FROM volunteers WHERE id=2";
+    const result = await pool.query(volTable);
+    res.json(result.rows);
+    console.log(result.rows);
+  } catch (error) {
+    console.error("Erreur dans /profil:", error);
+    res.status(500).json({ error: "Erreur serveur" });
+  }
 });
 module.exports = router;
 
-// router.get('/updateProfil', async (req, res) => {
-//       console.log("📩 Données reçues :", req.body); //
-//   try {
-//     const { firstname, lastname, location } = req.body;
-    
-//      // 1. Récupérer l'id de la ville à partir de son nom
-//     const vol = await pool.query(
-//       'SELECT id FROM volunteers WHERE firstname = Mohamed',
-//       [firstname]    
-//     );
+router.post("/updateProfil", async (req, res) => {
+  console.log("📩 Données reçues :", req.body);
+  try {
+    const { firstname, lastname, location } = req.body;
 
-//     if (vol.rows.length === 0) {
-//       return res.status(400).json({ error: 'Ville non trouvée' });
-//     }
+    const vol = await pool.query(
+      `UPDATE volunteers 
+  SET firstname = $1, lastname = $2, location = $3 
+  WHERE firstname = $4`,
+      [firstname, lastname, location, "Mohamed"]
+    );
 
-//     // const vol_id = vol.rows[0].id;
+    res.status(200).json({ message: "Collecte enregistrée avec succès." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: `Erreur lors de l'enregistrement.` });
+  }
+});
 
-//     // Exemple : insérer dans la base de données
-//     for (const item of volunteers) {
-//       await pool.query(
-//         'INSERT INTO volunteers (firstname, lastname, location) VALUES ($1, $2, $3)',
-//         [item.firstname, item.lastname, item.location]
-//       );
-//     }
-
-//     res.status(200).json({ message: 'Collecte enregistrée avec succès.' });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: `Erreur lors de l'enregistrement.` });
-//   }
-// });
-
-// module.exports = router;
+module.exports = router;
