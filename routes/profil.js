a3b4c7168cc03247c43212fb7e9da179af7c7af3
rerefenec <renaud.delorme@free.fr>
2025-06-05 12:19:09 +0200

    création-formulaire.js

--- routes/profil.js
+++ routes/profil.js
@@ -16,36 +16,3 @@ console.log(result.rows);
 });
 module.exports = router;
 
-// router.get('/updateProfil', async (req, res) => {
-//       console.log("📩 Données reçues :", req.body); //
-//   try {
-//     const { firstname, lastname, location } = req.body;
-    
-//      // 1. Récupérer l'id de la ville à partir de son nom
-//     const vol = await pool.query(
-//       'SELECT id FROM volunteers WHERE firstname = Mohamed',
-//       [firstname]    
-//     );
-
-//     if (vol.rows.length === 0) {
-//       return res.status(400).json({ error: 'Ville non trouvée' });
-//     }
-
-//     // const vol_id = vol.rows[0].id;
-
-//     // Exemple : insérer dans la base de données
-//     for (const item of volunteers) {
-//       await pool.query(
-//         'INSERT INTO volunteers (firstname, lastname, location) VALUES ($1, $2, $3)',
-//         [item.firstname, item.lastname, item.location]
-//       );
-//     }
-
-//     res.status(200).json({ message: 'Collecte enregistrée avec succès.' });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: `Erreur lors de l'enregistrement.` });
-//   }
-// });
-
-// module.exports = router;
